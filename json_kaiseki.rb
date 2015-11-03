55fcf8489c6dbb15707d9e260e87459ae65fe46a
kansadose <s1211076@edu.cc.uec.ac.jp>
2015-11-04 00:24:40 +0900

    ポジションを読み取り出力

--- json_kaiseki.rb
+++ json_kaiseki.rb
@@ -4,22 +4,27 @@ require 'json'
 #変数json_dataに引数で指定したfileを
 json_data = JSON.load(File.read(ARGV[0])) 
 
-data1 = json_data[0]
-show = data1['show']
+
+json_data.each do |datan|
+data = datan 
+show = data['show']
+time = show['time']
 ball = show['ball']
 left = show['left']
 right= show['right']
 
+#puts "#{time} (#{ball[0].to_i},#{ball[1].to_i})"
+puts "left"
+  left.each do |l|
+    puts "#{time} [#{l[0].to_i}] (#{l[1].to_i},#{l[2].to_i})"
+  end
+end
 
-p ball[0].to_i
-p left
-p right
 
 #data_hash = JSON.parse(file) 
 #jsonテキストをハッシュにパースするがデータが配列になっているのでうまくいかない。。。
 
 =begin
-
 file = File.read(ARGV[0])
 
 File.open("json_hash.txt","w") do |file|
@@ -29,4 +34,5 @@ end
 File.open("json_hash2.txt","w") do |file|
   JSON.load(fi,proc{|v| file.puts v })
 end
+
 =end
